fefcc33728ab90c9b5295afef5cefa0be29de070
faxriddinprof <faxriddinprof@github.com>
2025-12-23 01:26:08 +0500

    feat: Update ALLOWED_HOSTS in settings.py to allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z3*cs!%k)dpqr(^rqzky!orsk$qszyz#lh%rul&hloign-mjo9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
